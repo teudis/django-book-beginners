feb169477960f59388a2a79ca158ca81f7330d5e
Teudis Naranjo Ortiz <teudis2008@gmail.com>
2021-06-13 22:26:18 -0400

    forms adn user accounts

--- blog_project_django/blog_project/settings.py
+++ blog_project_django/blog_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static' )]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
